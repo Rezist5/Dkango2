657aa81a422af755444c769fdbbd2ffa66c42c22
Дудиков Иван <@itstep.loc>
2023-10-10 17:44:33 +0600

    Lesson_03

---
+++ stepshop/mainapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def products(request):
+    return render(request, 'products.html')
+def contact(request):
+    return render(request, 'contact.html')
+def product(request):
+    return render(request, 'single-product.html')
+

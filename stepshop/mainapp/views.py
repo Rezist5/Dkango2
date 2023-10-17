25ab3266b8475745ff2a11b27aa2f16a86ab96f3
Дудиков Иван <@itstep.loc>
2023-10-17 17:58:24 +0600

    awd

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
+
+
+
 def index(request):
     title = "Main"
     links_menu = [
@@ -18,23 +21,59 @@ def index(request):
 
 def about(request):
     title = "About"
-    context = {'title': title}
+    links_menu = [
+        {'link': 'index', 'name': "Главная"},
+        {'link': 'products:index', 'name': "Продукты"},
+        {'link': 'about', 'name': "О нас"},
+        {'link': 'contact', 'name': "Контакты"},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
     return render(request, 'about.html', context)
 
 
 def products(request):
     title = "Catalog"
-    context = {'title': title}
+    links_menu = [
+        {'link': 'index', 'name': "Главная"},
+        {'link': 'products:index', 'name': "Продукты"},
+        {'link': 'about', 'name': "О нас"},
+        {'link': 'contact', 'name': "Контакты"},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
     return render(request, 'products.html', context)
 
 
 def contact(request):
     title = "Contacts"
-    context = {'title': title}
+    links_menu = [
+        {'link': 'index', 'name': "Главная"},
+        {'link': 'products:index', 'name': "Продукты"},
+        {'link': 'about', 'name': "О нас"},
+        {'link': 'contact', 'name': "Контакты"},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
     return render(request, 'contact.html', context)
 
 
 def product(request):
     title = "Product page"
-    context = {'title': title}
+    links_menu = [
+        {'link': 'index', 'name': "Главная"},
+        {'link': 'products:index', 'name': "Продукты"},
+        {'link': 'about', 'name': "О нас"},
+        {'link': 'contact', 'name': "Контакты"},
+    ]
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
     return render(request, 'product.html', context)

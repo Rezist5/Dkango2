a51a3c8eb7466a9dd8250408f201a9a2e3dd931f
Дудиков Иван <@itstep.loc>
2023-11-14 17:42:21 +0600

    eight

--- stepshop/stepshop/urls.py
+++ stepshop/stepshop/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('about/', about, name="about"),
     path('contact/', contact, name="contact"),
     path('products', include('mainapp.urls', namespace="products")),
+    path('auth/', include('authapp.urls', namespace="auth")),
 ]
 
 if settings.DEBUG:
